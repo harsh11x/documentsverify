343357acfb50e8cf05dd786e61d8745cd7205558
Harsh Dev <harshdevsingh2004@gmail.com>
2026-04-23 09:55:34 +0530

    safer

--- next.config.js
+++ next.config.js
@@ -1,22 +1,4 @@
-const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD, PHASE_PRODUCTION_SERVER } = require("next/constants");
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-module.exports = (phase) => {
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {};
-
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      ...nextConfig,
-      distDir: ".next-dev"
-    };
-  }
-
-  if (phase === PHASE_PRODUCTION_BUILD || phase === PHASE_PRODUCTION_SERVER) {
-    return {
-      ...nextConfig,
-      distDir: ".next-build"
-    };
-  }
-
-  return nextConfig;
-};
+module.exports = nextConfig;
